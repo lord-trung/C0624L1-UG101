32327ddcd9148effed037a1bd791e70e18221669
Lord Trung <trungkoolls3@gmail.com>
2024-07-08 19:11:30 +0700

    Lesson 11: Hàm và phương thức trong C# (1)

---
+++ Lesson11/Method.cs
@@ -0,0 +1,13 @@
+namespace Lesson11;
+using System.Text;
+
+class Method
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+
+        
+    }
+}
