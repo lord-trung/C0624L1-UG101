3703f7e6aad57a485b00a649780d77602f15c0f6
Lord Trung <trungkoolls3@gmail.com>
2024-07-10 21:37:56 +0700

    Sừa bài 01

--- Lesson12/Bai01.cs
+++ Lesson12/Bai01.cs
@@ -31,49 +31,58 @@ class Bai01
 
     static void Menu()
     {
-        Console.WriteLine("---------------MENU---------------");
-        Console.Write($" 1: Thực hiện phép cộng.\n 2: Thực hiện phép trừ.\n 3: Thực hiện phép nhân.\n 4: Thực hiện phép chia.");
-        Console.WriteLine();
+        Console.WriteLine("-----------MENU-----------");
+        Console.WriteLine($" 1: Thực hiện phép cộng.");
+        Console.WriteLine($" 2: Thực hiện phép trừ.");
+        Console.WriteLine($" 3: Thực hiện phép nhân.");
+        Console.WriteLine($" 4: Thực hiện phép chia.");
         Console.Write("Chọn phép tính: ");
         string input = Console.ReadLine();
-      
+
         SelectMenu(input);
     }
 
     static void SelectMenu(string choice)
     {
-        bool check;
-        if (choice == "0" || choice == "1" || choice == "2" || choice == "3" || choice == "4")
+        if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
         {
-            check = true;
-        }
-        else check = false;
+            int selectedMenu;
+            int.TryParse(choice, out selectedMenu);
 
-        while (!check)
+            switch (selectedMenu)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    Menu();
+                    break;
+                case 1:
+                    Console.WriteLine("BẠN CHỌN PHÉP CỘNG");
+                    Menu();
+                    break;
+                case 2:
+                    Console.WriteLine("BẠN CHỌN PHÉP TRỪ");
+                    Menu();
+                    break;
+                case 3:
+                    Console.WriteLine("BẠN CHỌN PHÉP NHÂN");
+                    Menu();
+                    break;
+                case 4:
+                    Console.WriteLine("BẠN CHỌN PHÉP CHIA");
+                    Menu();
+                    break;
+            }
+        }
+        else
+        if (choice == "0")
         {
-            Menu();           
+            Environment.Exit(0);
         }
-
-        int selectedMenu;
-        int.TryParse(choice, out selectedMenu);
-
-        switch (selectedMenu)
+        else
         {
-            case 0:
-                Environment.Exit(0);
-                break;
-            case 1:
-                Console.WriteLine("BẠN CHỌN PHÉP CỘNG");
-                break;
-            case 2:
-                Console.WriteLine("BẠN CHỌN PHÉP TRỪ");
-                break;
-            case 3:
-                Console.WriteLine("BẠN CHỌN PHÉP NHÂN");
-                break;
-            case 4:
-                Console.WriteLine("BẠN CHỌN PHÉP CHIA");
-                break;
+            Menu();
         }
+
+
     }
 }
